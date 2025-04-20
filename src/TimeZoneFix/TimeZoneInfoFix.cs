32ff8f72bc6a1406ced3c9e1f8dea64881e33c12
Michaela Elschner <mimael@gmx.net>
2025-04-20 14:45:50 +0200

    add GetNextAdjustmentRule method

--- src/TimeZoneFix/TimeZoneInfoFix.cs
+++ src/TimeZoneFix/TimeZoneInfoFix.cs
@@ -26,6 +26,30 @@ public partial class TimeZoneInfoFix
     private DateTime ConvertFromUtc(DateTime dateTime, TimeSpan daylightDelta, TimeSpan baseUtcOffsetDelta) =>
         _timeZone.ConvertFromUtc(dateTime, daylightDelta, baseUtcOffsetDelta);
 
+    // TODO there may be cases where there is no next or previous adjustmentRule
     private TimeZoneInfo.AdjustmentRule GetPreviousAdjustmentRule(TimeZoneInfo.AdjustmentRule rule, int? ruleIndex) =>
         _timeZone.GetPreviousAdjustmentRule(rule, ruleIndex);
+    
+    private TimeZoneInfo.AdjustmentRule GetNextAdjustmentRule(TimeZoneInfo.AdjustmentRule rule, int? ruleIndex)
+    {
+        if (ruleIndex.HasValue && 0 < ruleIndex.GetValueOrDefault() && ruleIndex.GetValueOrDefault() < _adjustmentRules.Length)
+        {
+            return _adjustmentRules[ruleIndex.GetValueOrDefault() + 1];
+        }
+
+        TimeZoneInfo.AdjustmentRule result = rule;
+        for (int i = 1; i < _adjustmentRules.Length; i++)
+        {
+            // use ReferenceEquals here instead of AdjustmentRule.Equals because
+            // ReferenceEquals is much faster. This is safe because all the callers
+            // of GetPreviousAdjustmentRule pass in a rule that was retrieved from
+            // _adjustmentRules.  A different approach will be needed if this ever changes.
+            if (ReferenceEquals(rule, _adjustmentRules[i]))
+            {
+                result = _adjustmentRules[i + 1];
+                break;
+            }
+        }
+        return result;
+    }
 }
