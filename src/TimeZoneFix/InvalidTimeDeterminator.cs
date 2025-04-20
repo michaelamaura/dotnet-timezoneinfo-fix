07f1441f243913239e5ce3a87fc7a8f76a9ca2a8
Michaela Elschner <mimael@gmx.net>
2025-04-20 15:20:19 +0200

    implement test for Kiritimati1994 and matching code

--- src/TimeZoneFix/InvalidTimeDeterminator.cs
+++ src/TimeZoneFix/InvalidTimeDeterminator.cs
@@ -2,8 +2,28 @@ namespace TimeZoneFix;
 
 public static class InvalidTimeDeterminator
 {
-    public static (DateTime, DateTime) DetermineInvalidTime(TimeZoneInfo.AdjustmentRule? current, TimeZoneInfo.AdjustmentRule? next)
+    // probably needs to be able to return multiple ranges... ?
+    public static (DateTime From, DateTime To)[] DetermineInvalidTime(TimeZoneInfo.AdjustmentRule current,
+        TimeZoneInfo.AdjustmentRule? next)
     {
-        throw new NotImplementedException();
+        // TODO check for invalid ranges at end of adjustmentRule
+
+        // TODO only consider next if it's seamless
+
+        var nextBaseUtcOffsetDelta = next?.BaseUtcOffsetDelta ?? TimeSpan.Zero;
+        var currentBaseUtcOffsetDelta = current.BaseUtcOffsetDelta;
+
+        var baseUtcOffsetDeltaChange = nextBaseUtcOffsetDelta - currentBaseUtcOffsetDelta;
+
+        if (baseUtcOffsetDeltaChange > TimeSpan.Zero)
+        {
+            var endExclusive = current.DateEnd.AddDays(1);
+            var from = endExclusive - baseUtcOffsetDeltaChange;
+            var to = endExclusive;
+
+            return [(from, to)];
+        }
+
+        return [];
     }
 }
--- test/TimeZoneFix.Tests/InvalidTimeDeterminatorTests.cs
+++ test/TimeZoneFix.Tests/InvalidTimeDeterminatorTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Xunit.Abstractions;
 
 namespace TimeZoneFix.Tests;
@@ -6,7 +7,7 @@ namespace TimeZoneFix.Tests;
 public class InvalidTimeDeterminatorTests(ITestOutputHelper testOutputHelper)
 {
     [Fact]
-    public void DetermineInvalidTime()
+    public void DetermineInvalidTime_ForKiritimati1994()
     {
         var ruleKiritimati1994 = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
             dateStart: new DateTime(1994, 1, 1),
@@ -19,9 +20,12 @@ public class InvalidTimeDeterminatorTests(ITestOutputHelper testOutputHelper)
         testOutputHelper.WriteLine(ruleKiritimati1994.ToFormattedString());
 
         // from inclusive, to exclusive?
-        var (from, to) = InvalidTimeDeterminator.DetermineInvalidTime(ruleKiritimati1994, null);
-        from.Should().Be(new DateTime(1994, 12, 31,  0, 0, 0, DateTimeKind.Unspecified));
-        to.Should().Be(new DateTime(1995, 1, 1,  0, 0, 0, DateTimeKind.Unspecified));
+        var invalidInvalidTimes = InvalidTimeDeterminator.DetermineInvalidTime(ruleKiritimati1994, null);
+
+        using var _ = new AssertionScope();
+        invalidInvalidTimes.Should().HaveCount(1);
+        invalidInvalidTimes[0].From.Should().Be(new DateTime(1994, 12, 31, 0, 0, 0, DateTimeKind.Unspecified));
+        invalidInvalidTimes[0].To.Should().Be(new DateTime(1995, 1, 1, 0, 0, 0, DateTimeKind.Unspecified));
     }
 
     private static TimeZoneInfo.TransitionTime CreateFixedDateRule(TimeOnly timeOfDay, int month, int day) =>
