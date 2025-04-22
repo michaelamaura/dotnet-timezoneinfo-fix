1478c8172e13e9ad17cf0ebcc454a74525c3ce8a
Michaela Elschner <mimael@gmx.net>
2025-04-22 23:40:56 +0200

    continue with InvalidTimeDeterminator

--- src/TimeZoneFix/InvalidTimeDeterminator.cs
+++ src/TimeZoneFix/InvalidTimeDeterminator.cs
@@ -2,28 +2,35 @@ namespace TimeZoneFix;
 
 public static class InvalidTimeDeterminator
 {
+    
     // probably needs to be able to return multiple ranges... ?
-    public static (DateTime From, DateTime To)[] DetermineInvalidTime(TimeZoneInfo.AdjustmentRule current,
+    public static InvalidTimeRange[] DetermineInvalidTime(TimeZoneInfo.AdjustmentRule current,
         TimeZoneInfo.AdjustmentRule? next)
     {
+        // TODO ignore next adjustment rule if it is not seamless
+        
         // TODO check for invalid ranges at end of adjustmentRule
 
         // TODO only consider next if it's seamless
 
-        var nextBaseUtcOffsetDelta = next?.BaseUtcOffsetDelta ?? TimeSpan.Zero;
         var currentBaseUtcOffsetDelta = current.BaseUtcOffsetDelta;
+        var nextBaseUtcOffsetDelta = next?.BaseUtcOffsetDelta ?? TimeSpan.Zero;
 
         var baseUtcOffsetDeltaChange = nextBaseUtcOffsetDelta - currentBaseUtcOffsetDelta;
+        
+        // TODO also take DST changes into account
 
         if (baseUtcOffsetDeltaChange > TimeSpan.Zero)
         {
             var endExclusive = current.DateEnd.AddDays(1);
-            var from = endExclusive - baseUtcOffsetDeltaChange;
-            var to = endExclusive;
+            var formInclusive = endExclusive - baseUtcOffsetDeltaChange;
 
-            return [(from, to)];
+            return [new InvalidTimeRange(formInclusive, endExclusive)];
         }
+        // TODO if the delta change is negative, we have an amibuous time
 
         return [];
     }
+
+    public record struct InvalidTimeRange(DateTime FromInclusive, DateTime ToExclusive);
 }
--- test/TimeZoneFix.Tests/InvalidTimeDeterminatorTests.cs
+++ test/TimeZoneFix.Tests/InvalidTimeDeterminatorTests.cs
@@ -6,28 +6,141 @@ namespace TimeZoneFix.Tests;
 
 public class InvalidTimeDeterminatorTests(ITestOutputHelper testOutputHelper)
 {
-    [Fact]
-    public void DetermineInvalidTime_ForKiritimati1994()
-    {
-        var ruleKiritimati1994 = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
-            dateStart: new DateTime(1994, 1, 1),
-            dateEnd: new DateTime(1994, 12, 31),
-            daylightDelta: TimeSpan.Zero,
-            daylightTransitionStart: CreateFixedDateRule(TimeOnly.MinValue, 1, 1),
-            daylightTransitionEnd: CreateFixedDateRule(new TimeOnly(23, 59, 59, 999), 12, 31),
-            baseUtcOffsetDelta: TimeSpan.FromDays(-1));
+    private static readonly DateOnly DateOnly = new DateOnly(1, 1, 1);
 
-        testOutputHelper.WriteLine(ruleKiritimati1994.ToFormattedString());
+    public static
+        TheoryData<TimeZoneInfo.AdjustmentRule, TimeZoneInfo.AdjustmentRule?,
+            IReadOnlyCollection<InvalidTimeDeterminator.InvalidTimeRange>> InvalidTimeTestData => new()
+    {
+        // Kirimati, skipping 1993 to 1994 with not invalid time
+        {
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(1993, 1, 1),
+                dateEnd: new DateTime(1993, 12, 31),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.FromDays(-1),
+                daylightTransitionStart: CreateFixedDateRule(1, 1, TimeOnly.MinValue),
+                daylightTransitionEnd: CreateFixedDateRule(12, 31, new TimeOnly(23, 59, 59, 999))),
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(1994, 1, 1),
+                dateEnd: new DateTime(1994, 12, 31),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.FromDays(-1),
+                daylightTransitionStart: CreateFixedDateRule(1, 1, TimeOnly.MinValue),
+                daylightTransitionEnd: CreateFixedDateRule(12, 31, new TimeOnly(23, 59, 59, 999))),
+            []
+        },
+        // Kirimati, skipping 1994-12-31 completely
+        {
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(1994, 1, 1),
+                dateEnd: new DateTime(1994, 12, 31),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.FromDays(-1),
+                daylightTransitionStart: CreateFixedDateRule(1, 1, TimeOnly.MinValue),
+                daylightTransitionEnd: CreateFixedDateRule(12, 31, new TimeOnly(23, 59, 59, 999))),
+            null,
+            [
+                new InvalidTimeDeterminator.InvalidTimeRange(
+                    new DateTime(1994, 12, 31, 0, 0, 0, DateTimeKind.Unspecified),
+                    new DateTime(1995, 1, 1, 0, 0, 0, DateTimeKind.Unspecified))
+            ]
+        },
+        // Paraguay, last switch to DST
+        {
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(2024, 3, 24),
+                dateEnd: new DateTime(2024, 10, 6),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.FromHours(-1),
+                daylightTransitionStart: CreateFixedDateRule(3, 24, TimeOnly.MinValue),
+                daylightTransitionEnd: CreateFixedDateRule(10, 6, new TimeOnly(0, 59, 59, 999))),
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(2024, 10, 6),
+                dateEnd: new DateTime(2024, 10, 14),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.Zero,
+                daylightTransitionStart: CreateFixedDateRule(10, 6, new TimeOnly(1, 0)),
+                daylightTransitionEnd: CreateFixedDateRule(10, 14, new TimeOnly(23, 59, 59, 999))),
+            [
+                new InvalidTimeDeterminator.InvalidTimeRange(
+                    new DateTime(2024, 10, 6, 23, 0, 0, DateTimeKind.Unspecified),
+                    new DateTime(2024, 10, 7, 0, 0, 0, DateTimeKind.Unspecified))
+            ]
+        },
+        // Paraguay, last adjustment rule
+        {
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(2024, 10, 6),
+                dateEnd: new DateTime(2024, 10, 14),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.Zero,
+                daylightTransitionStart: CreateFixedDateRule(10, 6, new TimeOnly(1, 0)),
+                daylightTransitionEnd: CreateFixedDateRule(10, 14, new TimeOnly(23, 59, 59, 999))),
+            null,
+            []
+        },
+        // Portugal 1995 - this rule looks wrong, maybe there is also an issue with the parsing?
+        {
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(1995, 1, 1),
+                dateEnd: new DateTime(1995, 3, 26),
+                daylightDelta: TimeSpan.Zero,
+                baseUtcOffsetDelta: TimeSpan.FromHours(1),
+                daylightTransitionStart: CreateFixedDateRule(1, 1, TimeOnly.MinValue),
+                daylightTransitionEnd: CreateFixedDateRule(3, 26, new TimeOnly(0, 59, 59, 999))),
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(1995, 3, 26),
+                dateEnd: new DateTime(1995, 9, 24),
+                daylightDelta: TimeSpan.FromHours(2),
+                baseUtcOffsetDelta: TimeSpan.Zero,
+                daylightTransitionStart: CreateFixedDateRule(3, 26, new TimeOnly(1, 0)),
+                daylightTransitionEnd: CreateFixedDateRule(9, 24, new TimeOnly(2, 59, 59, 999))),
+            [
+                new InvalidTimeDeterminator.InvalidTimeRange(
+                    new DateTime(1995, 3, 26, 1, 0, 0, DateTimeKind.Unspecified),
+                    new DateTime(1995, 3, 26, 2, 0, 0, DateTimeKind.Unspecified))
+            ]
+        },
+        // Berlin 2024
+        {
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(2024, 3, 31),
+                dateEnd: new DateTime(2024, 10, 27),
+                daylightDelta: TimeSpan.FromHours(1),
+                baseUtcOffsetDelta: TimeSpan.Zero,
+                daylightTransitionStart: CreateFixedDateRule(3, 21, new TimeOnly(2, 0)),
+                daylightTransitionEnd: CreateFixedDateRule(10, 27, new TimeOnly(2, 59, 59, 999))),
+            TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(
+                dateStart: new DateTime(2025, 3, 30),
+                dateEnd: new DateTime(2025, 10, 26),
+                daylightDelta: TimeSpan.FromHours(1),
+                baseUtcOffsetDelta: TimeSpan.Zero,
+                daylightTransitionStart: CreateFixedDateRule(3, 30, new TimeOnly(1, 0)),
+                daylightTransitionEnd: CreateFixedDateRule(10, 26, new TimeOnly(2, 59, 59, 999))),
+            [
+                new InvalidTimeDeterminator.InvalidTimeRange(
+                    new DateTime(1995, 3, 26, 1, 0, 0, DateTimeKind.Unspecified),
+                    new DateTime(1995, 3, 26, 2, 0, 0, DateTimeKind.Unspecified))
+            ]
+        },
+    };
+
+
+    [Theory]
+    [MemberData(nameof(InvalidTimeTestData))]
+    public void DetermineInvalidTime(TimeZoneInfo.AdjustmentRule current, TimeZoneInfo.AdjustmentRule? next,
+        IReadOnlyCollection<InvalidTimeDeterminator.InvalidTimeRange> expected)
+    {
+        testOutputHelper.WriteLine(current.ToFormattedString());
 
         // from inclusive, to exclusive?
-        var invalidInvalidTimes = InvalidTimeDeterminator.DetermineInvalidTime(ruleKiritimati1994, null);
+        var invalidInvalidTimes = InvalidTimeDeterminator.DetermineInvalidTime(current, next);
 
         using var _ = new AssertionScope();
-        invalidInvalidTimes.Should().HaveCount(1);
-        invalidInvalidTimes[0].From.Should().Be(new DateTime(1994, 12, 31, 0, 0, 0, DateTimeKind.Unspecified));
-        invalidInvalidTimes[0].To.Should().Be(new DateTime(1995, 1, 1, 0, 0, 0, DateTimeKind.Unspecified));
+        invalidInvalidTimes.Should().BeEquivalentTo(expected);
     }
 
-    private static TimeZoneInfo.TransitionTime CreateFixedDateRule(TimeOnly timeOfDay, int month, int day) =>
-        TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(new DateOnly(1, 1, 1), timeOfDay), month, day);
+    private static TimeZoneInfo.TransitionTime CreateFixedDateRule(int month, int day, TimeOnly timeOfDay) =>
+        TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(DateOnly, timeOfDay), month, day);
 }
